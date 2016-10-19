b314a17ae83fa1920f39659e88cd59119ab283ea
cliu0507 <cliu0507@yahoo.com>
2016-10-19 01:40:21 -0700

    Load Input Training DataSet function

--- package/linear_regression/linear_regression.py
+++ package/linear_regression/linear_regression.py
@@ -6,6 +6,102 @@ import math
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+#Function loading training data
+#Possible arguments case1: x_file, y_file (row count of x_file == row count of y_file)
+#Possible arguments case2: x_file, y_file=None, y_position = -1. It means y is also inside x, y is the last element of each row
+def load_training_dataset(x_file, y_file=None, y_position=None, delimiter=","):
+    #Check if having y_file as argument
+    #Initialize x,y matrix
+    x = None
+    y = None
+    # Initial Num of Feature and Num of samples/examples
+    num_feature = 0
+    num_example = 0
+    if y_file == None and y_position == -1:
+        # Check whether inputfile exists
+        if not os.path.isfile(x_file):
+            raise Exception("Data example file %s is missing" % x_file)
+
+        # Initial X and Y(feature matrix and y matrix)
+        with open(x_file, 'r') as f:
+            for i, row in enumerate(f):
+                #If this row has non-digit character, skip this line
+                if re.search('[a-zA-Z]', row):
+                    continue
+                if x is not None:
+                    #Leave last element of each row as y value
+                    temparr = np.fromstring(row, dtype=float, sep=delimiter)
+                    line = temparr[:-1]
+                    y = np.append(y, temparr[-1])
+                    if num_feature == line.shape[0]:
+                        x = np.vstack((x, line))
+                    else:
+                        raise Exception("Input samples have different feature dimensions")
+                else:
+                    temparr = np.fromstring(row, dtype=float, sep=delimiter)
+                    x = temparr[:-1]
+                    y = np.array(temparr[-1])
+                    num_feature = (x.shape)[0]
+    elif y_file!=None and y_position == None:
+        if not os.path.isfile(x_file):
+            raise Exception("Data example file %s is missing" % x_file)
+
+        # Initial X (samples matrix)
+        with open(x_file, 'r') as f:
+            for i, row in enumerate(f):
+                #If this row has non-digit character, skip this line
+                if re.search('[a-zA-Z]', row):
+                    continue
+                if x is not None:
+                    line = np.fromstring(row, dtype=float, sep=',')
+                    if num_feature == line.shape[0]:
+                        x = np.vstack((x, line))
+                    else:
+                        raise Exception("Input samples have different feature dimensions")
+                else:
+                    x = np.fromstring(row, dtype=float, sep=',')
+                    num_feature = (x.shape)[0]
+
+        # Read y from data directory
+        with open(y_file, 'r') as f:
+            for row in f:
+                #If this row has non-digit character, skip this line
+                if re.search('[a-zA-Z]', row):
+                    continue
+                # Catch the exception if there are non numeric character in y file
+                try:
+                    float(row.rstrip())
+                except ValueError:
+                    print "y file has non-numeric character"
+
+                # Catch alphabet character
+                if re.search('[a-z][A-Z]]', row.rstrip()):
+                    raise Exception("y file has alphabet")
+
+                if y is not None:
+                    y = np.append(y, float(row.rstrip()))
+                else:
+                    y = np.array([float(row.rstrip())])
+
+        # Add dummy ones to matrix x for x0
+        x = np.insert(x, 0, 1, axis=1)
+        # Reshape x to normal shape and theta matrix
+        x = x.transpose()
+    else:
+        raise Exception("Wrong Argument while calling load_training_data function")
+    # Check if x or training dataset is empty
+    if x is not None:
+        num_example = x.shape[0]
+    else:
+        raise Exception("Training dataset file is empty")
+    # Add dummy ones to matrix x for x0
+    x = np.insert(x, 0, 1, axis=1)
+    # Reshape x to normal shape and theta matrix
+    x = x.transpose()
+
+    return x,y,num_example,num_feature
+
+
 
 #Feature Scaling Function: make xi approximately in the range of [-1,1]
 # x is (num_feature+1, num_example)
@@ -18,7 +114,6 @@ def feature_scaling_normalization(x , type , xMeans=None , xMin=None, xMax=None)
         xMax=np.max(x,axis=1)
         xMin=np.min(x,axis=1)
         num_feature=len(xMeans)
-        print xMeans
         #Exclude x0 (No. zero feature), since we set x0 = 1 for all examples
         for i in range(1,num_feature):
             x[i][:] = (x[i][:] - xMeans[i])/[xMax[i]-xMin[i]]
@@ -39,7 +134,7 @@ def feature_scaling_normalization(x , type , xMeans=None , xMin=None, xMax=None)
         x_scaled = np.insert(x,0,1,axis=1)
         return x_scaled
     else:
-        raise Exception("Wrong Input Arguments for feature_scaling_normalization function")
+        raise Exception("Wrong Input Arguments for load_training_dataset function")
 
 
 # x is ( num_feature+1 , num_example )
@@ -68,7 +163,6 @@ def gradientDescent(x, y, theta, m, alpha , numIterations):
         theta = theta_updated
         errorarr[i]=error
         print error
-        #print theta
     #Return theta, errorarr in different num_iteration and final minimum error(last element in errorarr)
     return theta,errorarr,errorarr[-1]
 
@@ -106,7 +200,6 @@ def gradientDescent2(x, y, theta, m, alpha , numIterations, lambda_value):
         theta = theta_updated
         errorarr[i]=error
         print error
-        #print theta
     #Return theta, errorarr in different num_iteration and final minimum error(last element in errorarr)
     return theta,errorarr,errorarr[-1]
 
@@ -127,6 +220,10 @@ def predict(theta, x_scaled):
     y_predict=np.dot(theta.transpose(),x_scaled.transpose())
     return y_predict
 
+
+
+
+
 #Current file absolute path
 cur_file=os.path.abspath(__file__)
 
@@ -139,60 +236,12 @@ while(not os.path.isdir(moving_dir + '/data')):
 data_dir=moving_dir + '/data'
 
 #Example files input filename and full path
-x_file=data_dir+'/input.csv'
-y_file=data_dir+'/y.csv'
+x_file=data_dir+'/winequality-red.csv'
+#y is at the end of each line in x_file or y can be in a separate file
+#y_file=data_dir+'/y.csv'
 
-#Check whether inputfile exists
-if not os.path.isfile(x_file):
-    raise Exception("Data example file %s is missing" %x_file )
-
-#Initial Num of Feature and Num of samples/examples
-num_feature=0
-num_example=0
-
-#Initial X (samples matrix)
-x = None
-with open(x_file,'r') as f:
-    for i, row in enumerate(f):
-        if x is not None:
-            temparr = np.fromstring(row,dtype=float, sep = ',')
-            if num_feature == temparr.shape[0]:
-                x = np.vstack((x,temparr))
-            else:
-                raise Exception("Input samples have different feature dimensions")
-        else:
-            x = np.fromstring(row,dtype=float, sep = ',')
-            num_feature = (x.shape)[0]
-if x is not None:
-    num_example=x.shape[0]
-else:
-    raise Exception("x (example matrix) is empty")
-
-#Initialize Y (samples value)
-y = None
-#Read y from data directory
-with open(y_file, 'r') as f:
-    for row in f:
-        #Catch the exception if there are non numeric character in y file
-        try:
-            float(row.rstrip())
-        except ValueError:
-            print "y file has non-numeric character"
-
-        #Catch alphabet character
-        if re.search( '[a-z][A-Z]]',row.rstrip()):
-            raise Exception("y file has alphabet")
-
-        if y is not None:
-            y = np.append(y,float(row.rstrip()))
-        else:
-            y = np.array([float(row.rstrip())])
-
-#Add dummy ones to matrix x for x0
-x = np.insert(x, 0 , 1, axis = 1)
-
-#Reshape x to normal shape and theta matrix
-x = x.transpose()
+#Load Training Dataset files
+x,y,num_example,num_feature = load_training_dataset(x_file, None, -1, delimiter=";")
 
 #x is a matrix with dimension (num_feature+1, num_example)
 #y is a vector with length num_example , y.shape() = (num_example,)
@@ -210,10 +259,12 @@ x,xMeans,xMax,xMin = feature_scaling_normalization(x , "training")
 
 #Call gradient descent function to Minimize J(theta)
 #(theta,_,_)=gradientDescent(x, y , theta, num_example, 0.01 , 50000)
-theta, errorarr, min_error = chart_iteration_cost( "false", x, y , theta, num_example , 0.05 , 200000)
+theta, errorarr, min_error = chart_iteration_cost( "false", x, y , theta, num_example , 0.5 , 500)
 
 #Call Predict function
-x = np.array([[3,4,5,6,-4], [4,5,5,6,5] , [-23,-4,5,2,7]])
+#[7.4,0.59,0.08,4.4,0.086,6,29,0.9974,3.38,0.5,9] -- 4
+#[8.6,0.42,0.39,1.8,0.068,6,12,0.99516,3.35,0.69,11.7] -- 8
+x = np.array([[7.4,0.59,0.08,4.4,0.086,6,29,0.9974,3.38,0.5,9],[8.6,0.42,0.39,1.8,0.068,6,12,0.99516,3.35,0.69,11.7]])
 x_scaled = feature_scaling_normalization(x , "predict" , xMeans, xMax, xMin)
 y_predict = predict(theta, x_scaled)
 print y_predict
